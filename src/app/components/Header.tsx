98c027ef1194a2755690f6511733b27d980a5186
Alokgit09 <130595475+Alokgit09@users.noreply.github.com>
2024-06-20 18:20:27 +0530

    fix padding

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = (props: Tprops) => {
 
   return (
     <header>
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-4 dark:bg-gray-800">
       <div className="text-right">
         {
           pathname == "/dashboard" ?
